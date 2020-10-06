b5710d5cc0fbad8dec219c62ce33133be0515c52
EC2 Default User <ec2-user@ip-172-31-41-123.ec2.internal>
2020-10-06 10:51:42 +0000

    start

---
+++ bookers2/config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+
+  root 'homes#top'
+  get 'home/about' => 'homes#about', as: 'about'
+  devise_for :users
+  resources :users,only: [:show,:index,:edit,:update]
+  resources :books
+  post 'follow/:id' => 'relationships#follow', as: 'follow' # フォローする
+  post 'unfollow/:id' => 'relationships#unfollow', as: 'unfollow' # フォロー外す
+end
